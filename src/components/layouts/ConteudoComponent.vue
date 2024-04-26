58320e12170488119c25937136429d1449b09e02
murilonicemento <murilo_nascimento22@outlook.com>
2024-04-25 22:47:27 -0300

    feat: create components Topo and Conteudo

---
+++ src/components/layouts/ConteudoComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div><p>Conteúdo</p></div>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/layouts/TopoComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div><p>Topo</p></div>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
