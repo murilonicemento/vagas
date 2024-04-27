b34591e7ff13dd3465c37155fc85a317f56cfdb8
murilonicemento <murilo_nascimento22@outlook.com>
2024-04-27 17:40:56 -0300

    feat: create component HomeView

---
+++ src/components/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template><div>HomeView</div></template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
