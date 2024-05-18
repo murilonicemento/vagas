8d5b8a04aa865a6022421123a9f14fcb1b5f27a1
murilonicemento <murilo_nascimento22@outlook.com>
2024-05-18 14:51:46 -0300

    feat: implements Home component

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,21 @@
       Desmontar o componente conteúdo
     </button>
     <ConteudoComponent v-if="visibilidade" />
+    <HomeView />
   </div>
 </template>
 
 <script>
 import ConteudoComponent from "./components/layouts/ConteudoComponent.vue";
 import TopoComponent from "./components/layouts/TopoComponent.vue";
+import HomeView from "./components/views/HomeView.vue";
 
 export default {
   name: "App",
   components: {
     ConteudoComponent,
     TopoComponent,
+    HomeView,
   },
   data: () => ({
     visibilidade: true,
