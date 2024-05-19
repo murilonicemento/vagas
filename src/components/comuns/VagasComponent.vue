8193bc627b80c667786684c6c353e6253473141c
murilonicemento <murilo_nascimento22@outlook.com>
2024-05-19 10:20:42 -0300

    list jobs

--- src/components/comuns/VagasComponent.vue
+++ src/components/comuns/VagasComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="card">
-    <div class="card-header bg-dark text-white">Título da vaga</div>
+    <div class="card-header bg-dark text-white">{{ titulo }}</div>
     <div class="card-body">
-      <p>Descrição truncada da vaga...</p>
+      <p>{{ descricao }}</p>
     </div>
     <div class="card-footer">
       <small class="text-muted"
-        >Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT |
-        Publicação: 10/11/2022</small
+        >Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo:
+        {{ tipo }} | Publicação: {{ publicacao }}</small
       >
     </div>
   </div>
@@ -16,6 +16,7 @@
 <script>
 export default {
   name: "VagasComponent",
+  props: ["titulo", "descricao", "salario", "modalidade", "tipo", "publicacao"],
 };
 </script>
 
