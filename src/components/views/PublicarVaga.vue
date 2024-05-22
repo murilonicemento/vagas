9c81c61d46fdedad8087125e40fcff4ccf90df10
murilonicemento <murilo_nascimento22@outlook.com>
2024-05-21 22:28:49 -0300

    feat: implements PublicarVaga component

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -1,6 +1,54 @@
 <template>
-  <div>
-    <h1>PublicarVaga</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais e de graça</h4>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Título da vaga</label>
+        <input type="text" class="form-control" />
+        <div class="form-text">
+          Por exemplo: Programador Javascript e VueJS.
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Descrição</label>
+        <textarea class="form-control"></textarea>
+        <div class="form-text">Informe os destalhes da vaga</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Salário</label>
+        <input type="number" class="form-control" />
+        <div class="form-text">Informe salário</div>
+      </div>
+      <div class="col">
+        <label for="" class="form-label">Modalidade</label>
+        <select class="form-select">
+          <option value="1">Home Office</option>
+          <option value="2">Presencial</option>
+        </select>
+        <div class="form-text">Informe onde as atividades serão realizadas</div>
+      </div>
+      <div class="col">
+        <label for="" class="form-label">Tipo</label>
+        <select class="form-select">
+          <option value="1">CLT</option>
+          <option value="2">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </div>
+    </div>
   </div>
 </template>
 
