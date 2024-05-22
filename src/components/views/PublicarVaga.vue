78d75da4c3b3e5bffffae5b498db9ab90a6a9204
murilonicemento <murilo_nascimento22@outlook.com>
2024-05-21 22:48:12 -0300

    feat: store mutiples register in localStorage

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -8,7 +8,7 @@
     <div class="row mt-3">
       <div class="col">
         <label for="" class="form-label">Título da vaga</label>
-        <input type="text" class="form-control" />
+        <input type="text" class="form-control" v-model="titulo" />
         <div class="form-text">
           Por exemplo: Programador Javascript e VueJS.
         </div>
@@ -17,19 +17,20 @@
     <div class="row mt-3">
       <div class="col">
         <label for="" class="form-label">Descrição</label>
-        <textarea class="form-control"></textarea>
+        <textarea class="form-control" v-model="descricao"></textarea>
         <div class="form-text">Informe os destalhes da vaga</div>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col">
         <label for="" class="form-label">Salário</label>
-        <input type="number" class="form-control" />
+        <input type="number" class="form-control" v-model="salario" />
         <div class="form-text">Informe salário</div>
       </div>
       <div class="col">
         <label for="" class="form-label">Modalidade</label>
-        <select class="form-select">
+        <select class="form-select" v-model="modalidade">
+          <option value="" disabled>Selecione</option>
           <option value="1">Home Office</option>
           <option value="2">Presencial</option>
         </select>
@@ -37,7 +38,8 @@
       </div>
       <div class="col">
         <label for="" class="form-label">Tipo</label>
-        <select class="form-select">
+        <select class="form-select" v-model="tipo">
+          <option value="" disabled>Selecione</option>
           <option value="1">CLT</option>
           <option value="2">PJ</option>
         </select>
@@ -46,7 +48,9 @@
     </div>
     <div class="row mt-3">
       <div class="col">
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
+        <button type="submit" class="btn btn-primary" @click="salvarVaga()">
+          Cadastrar
+        </button>
       </div>
     </div>
   </div>
@@ -55,6 +59,30 @@
 <script>
 export default {
   name: "PublicarVaga",
+  data: () => ({
+    titulo: "",
+    descricao: "",
+    salario: "",
+    modalidade: "",
+    tipo: "",
+  }),
+  methods: {
+    salvarVaga() {
+      let vagas = JSON.parse(localStorage.getItem("vagas"));
+
+      if (!vagas) vagas = [];
+
+      vagas.push({
+        titulo: this.titulo,
+        descricao: this.descricao,
+        salario: this.salario,
+        modalidade: this.modalidade,
+        tipo: this.tipo,
+      });
+
+      localStorage.setItem("vagas", JSON.stringify(vagas));
+    },
+  },
 };
 </script>
 
